89faa2c8417e56844bf66c2e2de8a67f5cc9ba64
Ram Kishor <krishnaenterprises0423@gmail.com>
2024-02-05 00:19:32 +0530

    ok

--- src/main/java/com/brijframework/app/ApplicaionMainApp.java
+++ src/main/java/com/brijframework/app/ApplicaionMainApp.java
@@ -3,12 +3,14 @@ package com.brijframework.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableSwagger2
+@EnableEurekaClient 
 public class ApplicaionMainApp {
     public static void main(String[] args) {
     	
